88deaf7983868cdf25bab27421e4f4848f4c8a0d
e chau <rizchau@qq.com>
2023-05-11 20:45:41 +0800

    add 2 lines

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '😁Correct Number!';
+
+document.querySelector('.number')=13
