3bb8cd42d7fbd0e1f1541b54b9b8b6cc78913cb1
e chau <rizchau@qq.com>
2023-05-15 13:23:29 +0800

    0515v2

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,11 +7,14 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 //staring conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
 
 //rolling dice func
 btnRoll.addEventListener('click', function () {
@@ -20,6 +23,14 @@ btnRoll.addEventListener('click', function () {
 
   //dsplay dice
   diceEl.classList.remove('hidden');
-  diceEl.scr = `dice-${dice}.png`;
-  //check for rolled
+  diceEl.src = `dice-${dice}.png`;
+
+  //check for rolled 1: true-switch to next player
+  if (dice !== 1) {
+    // add dice to current
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //switch to next player
+  }
 });
